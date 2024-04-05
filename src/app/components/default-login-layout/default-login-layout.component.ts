e360e765d8fcc014dd67f71c2c3afb1cdd4497f8
Waleska Bressan <waleskabressan20@gmail.com>
2024-04-05 15:51:44 -0300

    feat: creat login page

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -13,7 +13,6 @@ export class DefaultLoginLayoutComponent {
   @Input()  secondaryBtnText: string ="";
   @Input() disablePrimaryBtn: boolean = true;
   @Output("submit") onSubmit = new EventEmitter();
-
   @Output("navigate") onNavigate = new EventEmitter();
   
   submit(){
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,27 +2,48 @@ import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { LoginService } from '../../services/login.service'
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [DefaultLoginLayoutComponent, ReactiveFormsModule, PrimaryInputComponent ],
+  imports: [DefaultLoginLayoutComponent, ReactiveFormsModule, PrimaryInputComponent],
+  providers: [LoginService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm!: FormGroup;
-  constructor(){
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastrService: ToastrService,
+  ) {
     this.loginForm = new FormGroup({
-      email: new FormControl( 
-      '',
-      [Validators.required, Validators.email]
+      email: new FormControl(
+        '',
+        [Validators.required, Validators.email]
       ),
-      
+
       password: new FormControl(
-       '',
-      [Validators.required, Validators.minLength(8)]
+        '',
+        [Validators.required, Validators.minLength(8)]
       )
     })
+  }
+
+  submit() {
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastrService.success("Login feito com sucesso"),
+      error: () => this.toastrService.error ("Erro inesperado! Tente novamente"),
+    })
+  }
+
+  navigate() {
+    this.router.navigate(["/cadastro"])
   }
 }
