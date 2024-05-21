fef11034f235daa10240578483b428b9f2576bd3
Waleska Bressan <waleskabressan20@gmail.com>
2024-05-21 13:02:09 -0300

    add: ngx-mask

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PainelFinancasService } from './services/painel-financas.service';
+import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxMaskDirective, NgxMaskPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,7 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import {provideAnimations} from '@angular/platform-browser/animations'
 import { provideToastr } from 'ngx-toastr';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideEnvironmentNgxMask } from 'ngx-mask';
 
 
 export const appConfig: ApplicationConfig = {
@@ -14,5 +15,6 @@ export const appConfig: ApplicationConfig = {
     provideToastr(),
     provideAnimations(),
     provideHttpClient (withFetch()), provideAnimationsAsync(), 
+    provideEnvironmentNgxMask()
   ]
 };
